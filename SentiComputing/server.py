12194f432064509ac0541a2d67e3b724cddaf0ba
lixin77 <lixin77@mail2.sysu.edu.cn>
2015-09-28 08:45:22 +0800

    add the interface of index (main) page

--- SentiComputing/server.py
+++ SentiComputing/server.py
@@ -1,11 +1,16 @@
-from flask import Flask, request
+from flask import Flask, request, render_template, make_response, url_for, redirect
 
 app = Flask(__name__)
-
+app.debug = True
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    """
+    redirect to the index page
+    :return:
+    """
+    response = make_response(redirect('/index'))
+    return response
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
@@ -17,6 +22,13 @@ def upload():
         f = request.files['the_file']
         f.save('/var/www/uploads/test.txt')
 
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+    """
+    main page of the web application
+    :return:
+    """
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
